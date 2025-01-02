49dc0264f3fdb49bd280e3c4ca52159a9ca0e14e
Alice Chan <alicebay10@yahoo.co.uk>
2025-01-02 18:45:03 +0000

    added axios interceptos so that to keep users logges in for longer, normally up to 24 hours.

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,5 +1,8 @@
 import axios from "axios";
 
 axios.defaults.baseURL = 'https://drf-api-alice-c6521fcf7abe.herokuapp.com/';
-axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
-axios.defaults.withCredentials = true ;
+axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
+axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
