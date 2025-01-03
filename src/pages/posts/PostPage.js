106d095016e47689ca8bd49413a2e53f9ddb1d20
Alice Chan <alicebay10@yahoo.co.uk>
2025-01-03 21:36:12 +0000

    added like and comment icons to post. Post owner cannot like their own post, other user can like others post.

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -5,11 +5,11 @@ import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
 
 import appStyles from "../../App.module.css";
-import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import { useParams } from "react-router";
 import { axiosReq } from "../../api/axiosDefaults";
+import Post from "./Post";
 
 function PostPage() {
-  // Add your logic here
   const { id } = useParams();
   const [post, setPost] = useState({ results: [] });
 
@@ -33,10 +33,8 @@ function PostPage() {
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
         <p>Popular profiles for mobile</p>
-        <p>Post component</p>
-        <Container className={appStyles.Content}>
-          Comments
-        </Container>
+        <Post {...post.results[0]} setPosts={setPost} postPage />
+        <Container className={appStyles.Content}>Comments</Container>
       </Col>
       <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
         Popular profiles for desktop
