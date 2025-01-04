de7699aec5a538845ef12e9ada3f9a92a792a38d
Alice Chan <alicebay10@yahoo.co.uk>
2025-01-04 14:04:47 +0000

    added logic to display the list of posts.

--- src/App.js
+++ src/App.js
@@ -10,37 +10,46 @@ import PostPage from "./pages/posts/PostPage";
 import PostsPage from "./pages/posts/PostsPage";
 import { useCurrentUser } from "./contexts/CurrentUserContext";
 
-
 function App() {
   const currentUser = useCurrentUser();
-  const profile_id = currentUser?.profile_id || ""
+  const profile_id = currentUser?.profile_id || "";
 
   return (
     <div className={styles.App}>
       <NavBar />
       <Container className={styles.Main}>
         <Switch>
-          <Route exact path="/" render={() => (
-            <PostsPage message="No results found. Adjust the search keyword." />
-          )}
+          <Route
+            exact
+            path="/"
+            render={() => (
+              <PostsPage message="No results found. Adjust the search keyword." />
+            )}
           />
-          <Route exact path="/feed" render={() => (
-            <PostsPage message="No results found. Adjust the search keyword or follow a user."
-            filter={`owner__followed__owner__profile=${profile_id}&`}
-            />
-          )}
+          <Route
+            exact
+            path="/feed"
+            render={() => (
+              <PostsPage
+                message="No results found. Adjust the search keyword or follow a user."
+                filter={`owner__followed__owner__profile=${profile_id}&`}
+              />
+            )}
           />
-          <Route exact path="/liked" render={() => (
-            <PostsPage message="No results found. Adjust the search keyword or like a post."
-            filter={`likes__owner__profile=${profile_id}&ordering=-likes__created_at&`}
-            />
-          )}
+          <Route
+            exact
+            path="/liked"
+            render={() => (
+              <PostsPage
+                message="No results found. Adjust the search keyword or like a post."
+                filter={`likes__owner__profile=${profile_id}&ordering=-likes__created_at&`}
+              />
+            )}
           />
           <Route exact path="/signin" render={() => <SignInForm />} />
           <Route exact path="/signup" render={() => <SignUpForm />} />
           <Route exact path="/posts/create" render={() => <PostCreateForm />} />
-          <Route exact path="/posts/:id" render={()=> <PostPage />} />
-          
+          <Route exact path="/posts/:id" render={() => <PostPage />} />
           <Route render={() => <p>Page not found!</p>} />
         </Switch>
       </Container>
