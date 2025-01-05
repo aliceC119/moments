171ea1b53db0f88d2a5eae6067e99ea3431cd18d
Alice Chan <alicebay10@yahoo.co.uk>
2025-01-05 13:56:38 +0000

    added the MoreDropdown menu in the comment section but without the edit and delete function yet.

--- src/pages/comments/Comment.js
+++ src/pages/comments/Comment.js
@@ -3,9 +3,14 @@ import styles from "../../styles/Comment.module.css";
 import { Media } from 'react-bootstrap';
 import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 import Avatar from '../../components/Avatar';
+import { useCurrentUser } from '../../contexts/CurrentUserContext';
+import { MoreDropdown } from '../../components/MoreDropdown';
 
 const Comment = (props) => { 
     const { profile_id, profile_image, owner, updated_at, content} = props;
+
+    const currentUser = useCurrentUser()
+    const is_owner = currentUser?.username === owner;
 
   return (
     <div>
@@ -20,7 +25,9 @@ const Comment = (props) => {
         <p>{content}</p>
 
         </Media.Body>
-        
+        {is_owner && (
+            <MoreDropdown handleEdit={() => {}} handleDelete={() => {}} />
+        )}
         
         </Media>
         
