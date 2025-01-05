b3e3ad4f6c4eb9f640440eb9f3cf3c07123d36f1
Alice Chan <alicebay10@yahoo.co.uk>
2025-01-05 13:45:49 +0000

    added styles to the comment section.

--- src/pages/comments/Comment.js
+++ src/pages/comments/Comment.js
@@ -1,10 +1,31 @@
 import React from 'react'
 import styles from "../../styles/Comment.module.css";
+import { Media } from 'react-bootstrap';
+import { Link } from 'react-router-dom/cjs/react-router-dom.min';
+import Avatar from '../../components/Avatar';
 
-const Comment = (props) => {
+const Comment = (props) => { 
+    const { profile_id, profile_image, owner, updated_at, content} = props;
+
   return (
-    <div>Comment</div>
-  )
-}
+    <div>
+    <hr />
+    <Media>
+        <Link to={`/profiles/${profile_id}`}>
+            <Avatar src={profile_image} />
+        </Link>
+        <Media.Body className="aligh-self-center ml-2">
+        <span className={styles.Owner}>{owner}</span>
+        <span className={styles.Date}>{updated_at} </span>
+        <p>{content}</p>
 
-export default Comment
+        </Media.Body>
+        
+        
+        </Media>
+        
+        </div>
+  );
+};
+
+export default Comment;
