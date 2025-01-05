1f303b787a7ebe9a2b23dfd7a76ef0859a80de73
Alice Chan <alicebay10@yahoo.co.uk>
2025-01-05 15:36:29 +0000

    added a spinner when loading the most popular profiles list.

--- src/pages/profiles/PopularProfiles.js
+++ src/pages/profiles/PopularProfiles.js
@@ -1,48 +1,51 @@
-import React, { useEffect, useState } from 'react'
-import appStyles from '../../App.module.css'
-import { Container } from 'react-bootstrap'
-import { axiosReq } from '../../api/axiosDefaults';
-import { useCurrentUser } from '../../contexts/CurrentUserContext';
+import React, { useEffect, useState } from "react";
+import { Container } from "react-bootstrap";
+import { axiosReq } from "../../api/axiosDefaults";
+import appStyles from "../../App.module.css";
+import Asset from "../../components/Asset";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
 
 const PopularProfiles = () => {
-    const [profilesData, setProfileData] = useState({
-        // we will use the pageProfile later!
-        pageProfile: { results: [] },
-        popularProfiles: { results: [] },
-    });
-    const { popularProfiles } = profilesData;
-    const currentUser = useCurrentUser()
+  const [profileData, setProfileData] = useState({
+    // we will use the pageProfile later!
+    pageProfile: { results: [] },
+    popularProfiles: { results: [] },
+  });
+  const { popularProfiles } = profileData;
+  const currentUser = useCurrentUser();
 
-    useEffect(() => {
-        const handleMount = async () => {
-          try {
-            const { data } = await axiosReq.get(
-              "/profiles/?ordering=-followers_count"
-            );
-            setProfileData((prevState) => ({
-              ...prevState,
-              popularProfiles: data,
-            }));
-          } catch (err) {
-            console.log(err);
-          }
-        };
-    
-        handleMount();
-      }, [currentUser]);
+  useEffect(() => {
+    const handleMount = async () => {
+      try {
+        const { data } = await axiosReq.get(
+          "/profiles/?ordering=-followers_count"
+        );
+        setProfileData((prevState) => ({
+          ...prevState,
+          popularProfiles: data,
+        }));
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    handleMount();
+  }, [currentUser]);
 
   return (
     <Container className={appStyles.Content}>
-
-        
-        <p>Most followed profiles.</p>
-        {popularProfiles.results.map(profiles => (
-            <p key={profiles.id}>{profiles.owner}</p>
-        ))}
-
-
+      {popularProfiles.results.length ? (
+        <>
+          <p>Most followed profiles.</p>
+          {popularProfiles.results.map((profile) => (
+            <p key={profile.id}>{profile.owner}</p>
+          ))}
+        </>
+      ) : (
+        <Asset spinner />
+      )}
     </Container>
-  )
-}
+  );
+};
 
-export default PopularProfiles
+export default PopularProfiles;
