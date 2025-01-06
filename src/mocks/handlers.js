9b10a5cac191a110862b116b76a06199684dadfd
Alice Chan <alicebay10@yahoo.co.uk>
2025-01-06 13:16:43 +0000

    finished setting up for testing, a new folder 'mocks' inside th folder sr and a new file ' handlers.js' are created.

---
+++ src/mocks/handlers.js
@@ -0,0 +1,18 @@
+const basURL = "https://drf-api-alice-c6521fcf7abe.herokuapp.com/"
+export const handlers = [
+    rest.get(`${baseURL}dj-rest-auth/user/`, (req, res, ctx) => {
+        return res(
+            ctx.json({
+            pk: 4,
+            username: "Oscar",
+            email: "",
+            first_name: "",
+            last_name: "",
+            profile_id: 4,
+            profile_image: "https://res.cloudinary.com/dydlslgyc/image/upload/v1/media/images/european-shorthair-8601492_1280_pdxh60"
+            }))
+    }),
+    rest.post(`${baseURL}dj-rest-auth/logout/`, (req, res, ctx) => {
+        return res(ctx.status(200));
+    }),
+    ];
